c98e989ae21c6d397e7c74107a8930bf8d7068aa
Josh Lubaway <josh.lubaway@gmail.com>
2012-09-02 17:35:52 -0700

    Initial commit

---
+++ lib/long_url.rb
@@ -0,0 +1,87 @@
+require 'net/http'
+
+class LongUrl
+
+  def self.call(url)
+    new.call(url)
+  end
+
+  MAX_REDIRECTS = 5
+
+  def initialize
+    @redirects = 0
+  end
+
+  def call(url)
+    lengthen(url)
+  end
+
+  def lengthen(url)
+    result = send_request(url)
+
+    while result && result != url && @redirects < MAX_REDIRECTS
+      url = result
+      result = send_request(url)
+    end
+
+    result
+  end
+
+  def send_request(url)
+    uri = parse_uri(url)
+    request = Net::HTTP::Head.new(uri.path)
+    http = get_connection(uri)
+
+    begin
+      response = http.start { |http| http.request(request) }
+    rescue Timeout::Error
+      $stderr.puts "Timeout connecting to `#{uri.scheme}://#{uri.host}`"
+      exit(1)
+    rescue SocketError
+      $stderr.puts "It looks like you are not connected to the internet."
+      exit(1)
+    end
+
+    case response
+    when Net::HTTPMovedPermanently, Net::HTTPFound
+      @redirects += 1
+      response["Location"]
+    when Net::HTTPClientError, Net::HTTPServerError
+      nil
+    else
+      url
+    end
+  end
+
+  def parse_uri(url)
+    uri = URI.parse(url)
+
+    if uri.path == ""
+      uri.path = "/"
+    end
+
+    uri
+  end
+
+  def get_connection(uri)
+    http = Net::HTTP.new(uri.hostname, uri.port)
+
+    if uri.scheme == "https"
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.cert_store = get_cert_store
+    end
+
+    http.open_timeout = 1
+    http.read_timeout = 1
+
+    http
+  end
+
+  def get_cert_store
+    store = OpenSSL::X509::Store.new
+    store.set_default_paths
+    store
+  end
+
+end
---
+++ spec/long_url_spec.rb
@@ -0,0 +1,90 @@
+require 'webmock/rspec'
+require 'long_url'
+
+describe LongUrl do
+
+  # live test
+  # it 'Lengthens a URL' do
+  #   url = "http://bit.ly/O44xP7"
+  #   url = LongUrl.call(url)
+  #   url.should eq("https://github.com/jakobwesthoff/colorizer")
+  # end
+
+  it 'Lengthens a URL' do
+    url = "http://bit.ly/O44xP7"
+    stub_redirect(url, "https://github.com/jakobwesthoff/colorizer")
+    stub_request(:head, "https://github.com/jakobwesthoff/colorizer").to_return(:status => 200)
+
+    url = LongUrl.call(url)
+    url.should eq("https://github.com/jakobwesthoff/colorizer")
+  end
+
+  it "Lengthens on temporary redirects" do
+    url = "http://bit.ly/O44xP7"
+    stub_redirect(url, "https://github.com/jakobwesthoff/colorizer", 302)
+    stub_request(:head, "https://github.com/jakobwesthoff/colorizer").to_return(:status => 200)
+
+    url = LongUrl.call(url)
+    url.should eq("https://github.com/jakobwesthoff/colorizer")
+  end
+
+  it "does not lengthen a url that does not need to be lengthened" do
+    url = "http://bit.ly/O44xP7"
+    stub_request(:head, "http://bit.ly/O44xP7").to_return(:status => 200)
+
+    url = LongUrl.call(url)
+    url.should eql("http://bit.ly/O44xP7")
+  end
+
+  it "does not blow up on bad requests" do
+    stub_request(:head, "http://bit.ly/O44xP7").to_return(:status => 406)
+    url = LongUrl.call("http://bit.ly/O44xP7")
+    url.should be_nil
+  end
+
+  it "does not blow up on server errors" do
+    stub_request(:head, "http://bit.ly/O44xP7").to_return(:status => 500)
+    url = LongUrl.call("http://bit.ly/O44xP7")
+    url.should be_nil
+  end
+
+  it "should follow multiple redirects" do
+    stub_redirect("http://short.url/1", "http://short.url/2")
+    stub_redirect("http://short.url/2", "http://longurl.com/")
+    stub_request(:head, "http://longurl.com/").to_return(:status => 200)
+
+    result = LongUrl.call("http://short.url/1")
+    result.should eq("http://longurl.com/")
+  end
+
+  it "does not blow up on urls without a trailing slash" do
+    stub_redirect("http://short.url", "http://longurl.com")
+    stub_request(:head, "http://longurl.com").to_return(:status => 200)
+    result = LongUrl.call("http://short.url")
+    result.should eq("http://longurl.com")
+  end
+
+  # This isn't really testing anything =/ Just the abilty to mock ssl
+  it "does not blow up on https requests" do
+    stub_request(:head, "https://short.url/")
+    result = LongUrl.call("https://short.url/")
+  end
+
+  # it "will not follow redirects forever" do
+  #
+  # end
+
+  # it "determines if the final site is alive" do
+  #
+  # end
+
+  protected
+
+  def stub_redirect(url, location, response_code = 301)
+    stub_request(:head, url).to_return({
+      :status => response_code,
+      :headers => { "Location" => location }
+    })
+  end
+
+end
